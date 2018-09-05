9936e90e1f07b0fe4498a75595d4713b8882d27f
Manuv <Manuve@sg.ibm.com>
2018-09-06 03:23:01 +0800

    Establishing comunication with library

--- app/src/main/java/gemalto/com/gemaltouser/MainActivity.java
+++ app/src/main/java/gemalto/com/gemaltouser/MainActivity.java
@@ -1,8 +1,10 @@
 package gemalto.com.gemaltouser;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +15,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import gemalto.com.gemaltodatalib.dataprocessing.RetrieveData;
+import gemalto.com.gemaltodatalib.networking.response.genderquery.UserResult;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    AppCompatActivity mActivityObj;
+    List<UserResult> str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +32,25 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mActivityObj = this;
+        str = new ArrayList<>();
+        /*gemalto.com.gemaltodatalib.MainActivity mainActivity = new gemalto.com.gemaltodatalib.MainActivity();
+        mainActivity.checkConnection();*/
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        RetrieveData retrieveDataObj = new RetrieveData(mActivityObj);
+        //List<UserResult> str =   retrieveDataObj.initiateGenderQuery("female");
+        str = retrieveDataObj.initiateGenderQuery("female");
+        //Log.d("tag","Size of s=="+s.toString());
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                // Do something after 5s = 5000ms
+                Log.d("tag","Size of s in app=="+str.get(0).getGender().toString());
+
             }
-        });
+        }, 5000);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
