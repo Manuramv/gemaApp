89e594cb5cf676ae5ffa592fabdec7e0b693e71e
Manuv <Manuve@sg.ibm.com>
2018-09-07 01:16:34 +0800

    Query fragment implementation

--- app/src/main/java/gemalto/com/gemaltouser/fragments/QueryFragment.java
+++ app/src/main/java/gemalto/com/gemaltouser/fragments/QueryFragment.java
@@ -6,14 +6,23 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import gemalto.com.gemaltouser.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class QueryFragment extends android.support.v4.app.Fragment {
-
+public class QueryFragment extends android.support.v4.app.Fragment implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+    private Spinner spinner;
+    private static final String[] paths = {"Female","Male"};
+    private View mFragmentView;
+    private EditText etSeed,etMultiplUser;
+    private Button btnquery;
 
     public QueryFragment() {
         // Required empty public constructor
@@ -21,10 +30,44 @@ public class QueryFragment extends android.support.v4.app.Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_query, container, false);
+         mFragmentView = inflater.inflate(R.layout.fragment_query, container, false);
+
+        spinner = (Spinner)mFragmentView.findViewById(R.id.gender_dropdown);
+        etSeed = (EditText) mFragmentView.findViewById(R.id.et_seed);
+        etMultiplUser = (EditText) mFragmentView.findViewById(R.id.et_multiuser);
+        btnquery = (Button) mFragmentView.findViewById(R.id.btn_query);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        return mFragmentView;
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btn_query:
+                proceedWithDataQuery();
+             break;
+             default:
+                 break;
+        }
+    }
+
+    private void proceedWithDataQuery() {
+    }
 }
