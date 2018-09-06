5f1ee48836b2eff7ffc1edd8d8a7be544cca7141
Manuv <Manuve@sg.ibm.com>
2018-09-06 22:47:59 +0800

    Navigation drawer changes

--- app/src/main/java/gemalto/com/gemaltouser/MainActivity.java
+++ app/src/main/java/gemalto/com/gemaltouser/MainActivity.java
@@ -124,17 +124,13 @@ try {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_setting) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_queryuser) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_view_stored_user) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_exit) {
 
         }
 
